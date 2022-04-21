92c5c6e00c5187e079ebbc268ccda24c60ce633f
Andrew W. Harn <andrew.harn@broadcom.com>
2022-04-21 15:03:14 -0400

    Update logic to handle missing directories
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

--- scripts/merge.ts
+++ scripts/merge.ts
@@ -9,7 +9,7 @@
 *                                                                                       *
 */
 
-import { readdirSync, readFileSync, writeFileSync } from "fs";
+import { existsSync, readdirSync, readFileSync, writeFileSync } from "fs";
 import { join } from "path";
 import { jsonc as JSONC } from 'jsonc';
 
@@ -29,19 +29,21 @@ import { jsonc as JSONC } from 'jsonc';
     const commandTree = zoweTreeFileJSON.data;
 
     // Add the missing command groups
-    const commandGroupsFiles = readdirSync(commandGroupsDirectory);
-    for (const file of commandGroupsFiles) {
-        const commandGroup = JSONC.parse(readFileSync(join(commandGroupsDirectory, file)).toString());
-        let conflict = false;
-        for (const compareGroup of commandTree.children) {
-            if (compareGroup.name === commandGroup.name) { conflict = true; }
-        }
+    if (existsSync(commandGroupsDirectory)) {
+        const commandGroupsFiles = readdirSync(commandGroupsDirectory);
+        for (const file of commandGroupsFiles) {
+            const commandGroup = JSONC.parse(readFileSync(join(commandGroupsDirectory, file)).toString());
+            let conflict = false;
+            for (const compareGroup of commandTree.children) {
+                if (compareGroup.name === commandGroup.name) { conflict = true; }
+            }
 
-        if (!conflict) {
-            commandTree.children.push(commandGroup);
-            console.log("Command group " + commandGroup.name + " added successfully.");
-        } else {
-            console.log("Command group " + commandGroup.name + " was not added due to a conflicting command group.");
+            if (!conflict) {
+                commandTree.children.push(commandGroup);
+                console.log("Command group " + commandGroup.name + " added successfully.");
+            } else {
+                console.log("Command group " + commandGroup.name + " was not added due to a conflicting command group.");
+            }
         }
     }
 
@@ -50,78 +52,88 @@ import { jsonc as JSONC } from 'jsonc';
         if (commandGroup.name === "profiles") {
             for (const child of commandGroup.children) {
                 if (child.name === "create") {
-                    const files = readdirSync(profilesCreateDirectory);
-                    for (const file of files) {
-                        const profile = JSONC.parse(readFileSync(join(profilesCreateDirectory, file)).toString());
-                        let conflict = false;
-                        for (const compareProfile of child.children) {
-                            if (profile.name === compareProfile.name) { conflict = true; }
-                        }
-                        if (!conflict) {
-                            child.children.push(profile);
-                            console.log("Profile " + profile.name + " was added to profiles create successfully.");
-                        } else {
-                            console.log("Profile " + profile.name + " was not added to profiles create due to a conflicting profile.");
+                    if (existsSync(profilesCreateDirectory)) {
+                        const files = readdirSync(profilesCreateDirectory);
+                        for (const file of files) {
+                            const profile = JSONC.parse(readFileSync(join(profilesCreateDirectory, file)).toString());
+                            let conflict = false;
+                            for (const compareProfile of child.children) {
+                                if (profile.name === compareProfile.name) { conflict = true; }
+                            }
+                            if (!conflict) {
+                                child.children.push(profile);
+                                console.log("Profile " + profile.name + " was added to profiles create successfully.");
+                            } else {
+                                console.log("Profile " + profile.name + " was not added to profiles create due to a conflicting profile.");
+                            }
                         }
                     }
                 } else if (child.name === "delete") {
-                    const files = readdirSync(profilesDeleteDirectory);
-                    for (const file of files) {
-                        const profile = JSONC.parse(readFileSync(join(profilesDeleteDirectory, file)).toString());
-                        let conflict = false;
-                        for (const compareProfile of child.children) {
-                            if (profile.name === compareProfile.name) { conflict = true; }
-                        }
-                        if (!conflict) {
-                            child.children.push(profile);
-                            console.log("Profile " + profile.name + " was added to profiles delete successfully.");
-                        } else {
-                            console.log("Profile " + profile.name + " was not added to profiles delete due to a conflicting profile.");
+                    if (existsSync(profilesDeleteDirectory)) {
+                        const files = readdirSync(profilesDeleteDirectory);
+                        for (const file of files) {
+                            const profile = JSONC.parse(readFileSync(join(profilesDeleteDirectory, file)).toString());
+                            let conflict = false;
+                            for (const compareProfile of child.children) {
+                                if (profile.name === compareProfile.name) { conflict = true; }
+                            }
+                            if (!conflict) {
+                                child.children.push(profile);
+                                console.log("Profile " + profile.name + " was added to profiles delete successfully.");
+                            } else {
+                                console.log("Profile " + profile.name + " was not added to profiles delete due to a conflicting profile.");
+                            }
                         }
                     }
                 } else if (child.name === "list") {
-                    const files = readdirSync(profilesListDirectory);
-                    for (const file of files) {
-                        const profile = JSONC.parse(readFileSync(join(profilesListDirectory, file)).toString());
-                        let conflict = false;
-                        for (const compareProfile of child.children) {
-                            if (profile.name === compareProfile.name) { conflict = true; }
-                        }
-                        if (!conflict) {
-                            child.children.push(profile);
-                            console.log("Profile " + profile.name + " was added to profiles list successfully.");
-                        } else {
-                            console.log("Profile " + profile.name + " was not added to profiles list due to a conflicting profile.");
+                    if (existsSync(profilesListDirectory)) {
+                        const files = readdirSync(profilesListDirectory);
+                        for (const file of files) {
+                            const profile = JSONC.parse(readFileSync(join(profilesListDirectory, file)).toString());
+                            let conflict = false;
+                            for (const compareProfile of child.children) {
+                                if (profile.name === compareProfile.name) { conflict = true; }
+                            }
+                            if (!conflict) {
+                                child.children.push(profile);
+                                console.log("Profile " + profile.name + " was added to profiles list successfully.");
+                            } else {
+                                console.log("Profile " + profile.name + " was not added to profiles list due to a conflicting profile.");
+                            }
                         }
                     }
                 } else if (child.name === "set-default") {
-                    const files = readdirSync(profilesSetDefaultDirectory);
-                    for (const file of files) {
-                        const profile = JSONC.parse(readFileSync(join(profilesSetDefaultDirectory, file)).toString());
-                        let conflict = false;
-                        for (const compareProfile of child.children) {
-                            if (profile.name === compareProfile.name) { conflict = true; }
-                        }
-                        if (!conflict) {
-                            child.children.push(profile);
-                            console.log("Profile " + profile.name + " was added to profiles set-default successfully.");
-                        } else {
-                            console.log("Profile " + profile.name + " was not added to profiles set-default due to a conflicting profile.");
+                    if (existsSync(profilesSetDefaultDirectory)) {
+                        const files = readdirSync(profilesSetDefaultDirectory);
+                        for (const file of files) {
+                            const profile = JSONC.parse(readFileSync(join(profilesSetDefaultDirectory, file)).toString());
+                            let conflict = false;
+                            for (const compareProfile of child.children) {
+                                if (profile.name === compareProfile.name) { conflict = true; }
+                            }
+                            if (!conflict) {
+                                child.children.push(profile);
+                                console.log("Profile " + profile.name + " was added to profiles set-default successfully.");
+                            } else {
+                                console.log("Profile " + profile.name + " was not added to profiles set-default due to a conflicting profile.");
+                            }
                         }
                     }
                 } else if (child.name === "update") {
-                    const files = readdirSync(profilesUpdateDirectory);
-                    for (const file of files) {
-                        const profile = JSONC.parse(readFileSync(join(profilesUpdateDirectory, file)).toString());
-                        let conflict = false;
-                        for (const compareProfile of child.children) {
-                            if (profile.name === compareProfile.name) { conflict = true; }
-                        }
-                        if (!conflict) {
-                            child.children.push(profile);
-                            console.log("Profile " + profile.name + " was added to profiles update successfully.");
-                        } else {
-                            console.log("Profile " + profile.name + " was not added to profiles update due to a conflicting profile.");
+                    if (existsSync(profilesUpdateDirectory)) {
+                        const files = readdirSync(profilesUpdateDirectory);
+                        for (const file of files) {
+                            const profile = JSONC.parse(readFileSync(join(profilesUpdateDirectory, file)).toString());
+                            let conflict = false;
+                            for (const compareProfile of child.children) {
+                                if (profile.name === compareProfile.name) { conflict = true; }
+                            }
+                            if (!conflict) {
+                                child.children.push(profile);
+                                console.log("Profile " + profile.name + " was added to profiles update successfully.");
+                            } else {
+                                console.log("Profile " + profile.name + " was not added to profiles update due to a conflicting profile.");
+                            }
                         }
                     }
                 }
